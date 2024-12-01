535514dd6da11dd3f8b55bde96ea08e889a1c195
MIIGUEELC <miigueel9219@gmail.com>
2024-12-01 20:30:30 +0100

    fotos y slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -28,3 +28,99 @@ const hideNavBar = () => {
 }
 
 window.addEventListener('scroll', hideNavBar);
+
+
+// Slider for rooms sections (Home)
+const swiper = new Swiper(".swiper", {
+  // Necessary parameters
+  effect: "slide",
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 6000,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+
+// Slider for features section (Home)
+const swiperFeatures = new Swiper(".swiperFeatures", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: "1",
+  spaceBetween: 30,
+  autoplay: {
+    delay: 6000,
+  },
+  pagination: {
+    el: ".swiperFeatures-pagination",
+    type: "bullets",
+  },
+});
+
+
+// Slider for menu section (Home)
+const swiperMenu = new Swiper(".swiperMenu", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: "1",
+  spaceBetween: 30,
+  autoplay: {
+    delay: 6000,
+  },
+  navigation: {
+    nextEl: ".swiperMenu-button-next",
+    prevEl: ".swiperMenu-button-prev",
+  },
+  breakpoints: {
+    1000: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+});
+
+
+// Slider for secondary menu section (Home)
+const swiperMenuImages = new Swiper(".swiperMenuImages", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: "1",
+  spaceBetween: 30,
+  autoplay: {
+    delay: 6000,
+  },
+  pagination: {
+    el: ".swiperMenuImages-pagination",
+    type: "bullets",
+  },
+});
+
+
+// Slider for rooms list (rooms.html)
+const swiperRoomsList = new Swiper(".swiperRoomsList", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: "1",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiperRoomsList-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
